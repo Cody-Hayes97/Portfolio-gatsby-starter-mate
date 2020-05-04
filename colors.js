482f951541d5a07823fe03fd5cc4e8771dd35f8f
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-05-04 13:12:45 -0700

    structure

---
+++ colors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  background: 'light-gray',
+  backgroundDark: '#62e2ff',
+
+  text: 'black',
+
+  primary: 'black',
+  primaryLight: 'black',
+  primaryDark: '#004db3',
+
+  secondary: '#004db3',
+  secondaryLight: 'blue',
+  secondaryDark: 'black',
+};
